4de8f2c28c9c0c172f38a3a87d88285c8781ef73
Nico-Gabriel Ruckendorfer <nicogabrielruckendorfer@gmail.com>
2022-10-13 20:59:02 +0200

    renamed SettingsButton to SettingsButtonView; changed code to simplify it and reduce the amount of parameters needed for SettingsButtonView

--- Weather/ContentView.swift
+++ Weather/ContentView.swift
@@ -18,8 +18,7 @@ struct ContentView: View {
             BackgroundView(lightMode: lightMode)
             VStack {
                 Spacer()
-                SettingsButton(textColor: lightMode ? lightblue : .white,
-                        backgroundColor: lightMode ? .white : lightgray)
+                SettingsButtonView(lightMode: lightMode)
             }
         }
     }
@@ -30,25 +29,23 @@ struct BackgroundView: View {
     let lightMode: Bool
 
     var body: some View {
-        let colors = lightMode ? [lightblue, lightpink] : [darkgray, lightgray]
-        LinearGradient(gradient: Gradient(colors: colors),
+        LinearGradient(gradient: Gradient(colors: lightMode ? [lightblue, lightpink] : [darkgray, lightgray]),
                 startPoint: .topLeading,
                 endPoint: .bottomTrailing)
                 .edgesIgnoringSafeArea(.all)
     }
 }
 
-struct SettingsButton: View {
+struct SettingsButtonView: View {
 
-    var textColor: Color
-    var backgroundColor: Color
+    let lightMode: Bool
 
     var body: some View {
         NavigationLink("Settings", destination: SettingsView())
                 .font(.system(size: 20, weight: .bold, design: .default))
                 .frame(width: deviceWidth - 120, height: 50)
-                .foregroundColor(textColor)
-                .background(backgroundColor)
+                .foregroundColor(lightMode ? lightblue : .white)
+                .background(lightMode ? .white : lightgray)
                 .cornerRadius(10)
                 .padding(.bottom, 30)
     }
