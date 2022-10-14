59b55d43d74b4539dea47c6e45685af5165f4d95
Nico-Gabriel Ruckendorfer <nicogabrielruckendorfer@gmail.com>
2022-10-14 14:10:09 +0200

    added headers for settings; added toggle in settings

--- Weather/SettingsView.swift
+++ Weather/SettingsView.swift
@@ -4,16 +4,31 @@ struct SettingsView: View {
 
     @Binding var selectedColorScheme: Int
 
+    @State var useLocation = true
+
     var body: some View {
         Form {
-            List {
+            Section(header: SectionHeaderTextView(text: "General settings")) {
                 Picker("Appearance", selection: $selectedColorScheme) {
                     Text("System default").tag(0)
                     Text("Light mode").tag(1)
                     Text("Dark mode").tag(2)
                 }
             }
+            Section(header: SectionHeaderTextView(text: "Location")) {
+                Toggle("Use location", isOn: $useLocation)
+            }
         }
                 .navigationTitle("Settings")
+    }
+}
+
+struct SectionHeaderTextView: View {
+
+    let text: String
+
+    var body: some View {
+        Text(text)
+                .padding(.leading, -20)
     }
 }
