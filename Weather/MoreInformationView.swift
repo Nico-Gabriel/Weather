dc4b178903176dea01642dcb366bf3f44389f9d6
Nico-Gabriel Ruckendorfer <nicogabrielruckendorfer@gmail.com>
2022-11-04 17:57:34 +0100

    added simple view to MoreInformationView

--- Weather/MoreInformationView.swift
+++ Weather/MoreInformationView.swift
@@ -3,6 +3,15 @@ import SwiftUI
 struct MoreInformationView: View {
 
     var body: some View {
-        Text("More information ...")
+        VStack {
+            Text("More information")
+                    .font(.title)
+                    .padding(50)
+            Text("Information comes here")
+                    .padding(50)
+            Button("Dismiss") {
+                print("dismiss pressed")
+            }
+        }
     }
 }
