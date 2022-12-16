ce6e14442638436c0c3f4756840088326f42a2a3
Nico-Gabriel Ruckendorfer <nicogabrielruckendorfer@gmail.com>
2022-12-16 22:56:45 +0100

    worked on AboutThisAppView.swift

--- Weather/AboutThisAppView.swift
+++ Weather/AboutThisAppView.swift
@@ -7,9 +7,82 @@ struct AboutThisAppView: View {
     var body: some View {
         NavigationView {
             ScrollView {
-                Text("abc123...")
+                LazyVStack (alignment: .leading) {
+                    Text("Weather")
+                            .font(.largeTitle)
+                            .bold()
+                }
+                        .padding()
+                Divider()
+                        .padding()
+                LazyVStack (alignment: .leading) {
+                    Text("Subheadline")
+                            .font(.title)
+                            .fontWeight(.semibold)
+                    Text("Text 1...")
+                    Text("Text 2...")
+                    Text("Text 3...")
+                    Text("Text 4...")
+                    Text("Text 5...")
+                    Text("Text 6...")
+                    Text("Text 7...")
+                    Text("Text 8...")
+                    Text("Text 9...")
+                }
+                        .padding()
+                Divider()
+                        .padding()
+                LazyVStack (alignment: .leading) {
+                    Text("Subheadline")
+                            .font(.title)
+                            .fontWeight(.semibold)
+                    Text("Text 1...")
+                    Text("Text 2...")
+                    Text("Text 3...")
+                    Text("Text 4...")
+                    Text("Text 5...")
+                    Text("Text 6...")
+                    Text("Text 7...")
+                    Text("Text 8...")
+                    Text("Text 9...")
+                }
+                        .padding()
+                Divider()
+                        .padding()
+                LazyVStack (alignment: .leading) {
+                    Text("Subheadline")
+                            .font(.title)
+                            .fontWeight(.semibold)
+                    Text("Text 1...")
+                    Text("Text 2...")
+                    Text("Text 3...")
+                    Text("Text 4...")
+                    Text("Text 5...")
+                    Text("Text 6...")
+                    Text("Text 7...")
+                    Text("Text 8...")
+                    Text("Text 9...")
+                }
+                        .padding()
+                Divider()
+                        .padding()
+                LazyVStack (alignment: .leading) {
+                    Text("Developer")
+                            .font(.title)
+                            .fontWeight(.semibold)
+                    Text("Text 1...")
+                    Text("Text 2...")
+                    Text("Text 3...")
+                    Text("Text 4...")
+                    Text("Text 5...")
+                    Text("Text 6...")
+                    Text("Text 7...")
+                    Text("Text 8...")
+                    Text("Text 9...")
+                }
+                        .padding()
             }
-                    .navigationTitle("About this app")
+                    .navigationBarTitle("About this app", displayMode: .inline)
                     .toolbar {
                         ToolbarItem {
                             Button("Close") {
